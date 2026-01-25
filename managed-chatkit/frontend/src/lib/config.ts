77f8bbde4c5ab3aceeaddd0b4969eacc80d16744
Brandwithin-admin <hq@brandwithin.com>
2026-01-25 16:03:50 +0800

    Update config.ts

--- managed-chatkit/frontend/src/lib/config.ts
+++ managed-chatkit/frontend/src/lib/config.ts
@@ -1,7 +1,6 @@
 import { ColorScheme, StartScreenPrompt, ThemeOption } from "@openai/chatkit";
 
-export const WORKFLOW_ID =
-  process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() ?? "";
+export const WORKFLOW_ID = import.meta.env.VITE_CHATKIT_WORKFLOW_ID;?.trim() ?? "";
 
 export const CREATE_SESSION_ENDPOINT = "/api/create-session";
 
